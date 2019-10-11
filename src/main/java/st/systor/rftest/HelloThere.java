c01624351918a614b1341de297980e9aed62202b
Roar Foshaug <roar.foshaug@systor.st>
2019-10-11 15:28:01 +0200

    x

--- src/main/java/st/systor/rftest/HelloThere.java
+++ src/main/java/st/systor/rftest/HelloThere.java
@@ -3,7 +3,7 @@ package st.systor.rftest;
 public class HelloThere {
 	
 	public static void main (String[] args) {
-		throw new RuntimeException("Hello There!");
+		System.out.println("Hello There!");
 	}
 
 }
